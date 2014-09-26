4995563d2226cb6ffb840f87082893210b69beb2
reinerwaldmann <mksd@list.ru>
2014-09-26 17:05:45 +0400

    инструёкции

--- MiramisPHP/index1.php
+++ MiramisPHP/index1.php
@@ -5,11 +5,22 @@ include ("htmlgeneral.php");
 
 echo makehead("MIRAMIS: главная страница");
 
+
 //echo "<img src='images/logo_sktb.png'>";
 
-//echo "<h1>MIRAMIS </h1>";
+echo "<h1>MIRAMIS </h1>";
 
 echo "<h2>Главная страница</h2>";
+
+
+include ('User_manual.html');
+
+echo makefoot();
+die();
+
+
+
+
 ///echo "<a href='protocols.php'> Обзор протоколов </a> &nbsp&nbsp&nbsp&nbsp";
 //echo "<a href='results.php'> Обзор результатов </a>";
 
